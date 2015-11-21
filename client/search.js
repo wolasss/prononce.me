2538c09b17ec4399851a337a1ac4142c7f29ad83
Adam Wolski <wolas.abc@gmail.com>
2015-11-21 11:49:06 +0100

    helper keyboard

--- client/search.js
+++ client/search.js
@@ -14,11 +14,22 @@ Template.ytSearch.events({
 		if(e.which === 13) {
 			searchQuery();
 		}
+	},
+	'click .char-picker': function(e) {
+		var c = $(e.currentTarget).text();
+		var $input = $('.query');
+		var caretPos = $input[0].selectionStart;
+		var currentVal = $input.val();
+
+		$input.val( currentVal.substring(0, caretPos) + c + currentVal.substring(caretPos) );
 	}
 });
 
 Template.ytSearch.helpers({
 	getCurrentQuery: function() {
 		return Session.get("currentQuery");
+	},
+	char: function() {
+		return ["ù", "û", "ü", "ÿ", "à", "â", "æ", "ç", "é", "è", "ê", "ë", "ï", "î", "ô", "œ"]; //should should be external and language specific
 	}
 });
