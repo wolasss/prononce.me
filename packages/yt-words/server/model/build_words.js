7dabb1544580fd3404bf52a08852cb03a2c5478f
Adam Wolski <wolas.abc@gmail.com>
2015-12-11 14:42:41 +0100

    build words algo change

--- packages/yt-words/server/model/build_words.js
+++ packages/yt-words/server/model/build_words.js
@@ -2,14 +2,13 @@ YT_APP.buildWordsCollection = function() {
 
 	YT_APP.Captions.find({}).forEach(function (doc){
 		if(doc.text) {
-			var words = doc.text.split(" ");
+			var words = doc.text.replace(/[\n\r\s]/gi, " ").split(" ");
 			_.each(words, function(word){
-				word = word.replace(/[\.\,\?<\>\/\\\s\!\-\=\[\]\d]/gi, "");
-				var normalized = YT_APP.NormalizeAccents(word);
+				var normalized = YT_APP.NormalizeAccents(word.replace(/[\.\,\?<\>\/\\\s\!\-\=\[\]\'\"\:\(\)\d]/gi, ""));
 
 				if(normalized) {					
 					if(!YT_APP.Words.findOne({normalized: normalized})) {
-						YT_APP.Words.insert({text: word, normalized: normalized});
+						YT_APP.Words.insert({text: word.replace(/[\.\,\?\<\>\!]/gi, ""), normalized: normalized});
 					}
 				}
 				
