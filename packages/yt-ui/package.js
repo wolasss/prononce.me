fd62eb0543c3d65cfd2ea1cc67d486722882839c
Adam Wolski <wolas.abc@gmail.com>
2016-11-28 23:06:52 +0100

    update meteor to the latest version

--- packages/yt-ui/package.js
+++ packages/yt-ui/package.js
@@ -1,9 +1,31 @@
 Package.describe({
-  summary: "YouTube app ui"
+	summary: "YouTube app ui"
 });
 
-Package.on_use(function (api) {
+Package.on_use(function(api) {
 	api.use(['fourseven:scss', 'twbs:bootstrap'], ['client']);
-	
+
+	api.addFiles(['styles/font-awesome/_variables.scss',
+		'styles/font-awesome/_animated.scss',
+		'styles/font-awesome/_bordered-pulled.scss',
+		'styles/font-awesome/_core.scss',
+		'styles/font-awesome/_fixed-width.scss',
+		'styles/font-awesome/_icons.scss',
+		'styles/font-awesome/_larger.scss',
+		'styles/font-awesome/_list.scss',
+		'styles/font-awesome/_mixins.scss',
+		'styles/font-awesome/_path.scss',
+		'styles/font-awesome/_rotated-flipped.scss',
+		'styles/font-awesome/_stacked.scss',
+		'styles/font-awesome/_variables.scss'
+	], 'client', {
+		isImport: true
+	});
+	api.addFiles(['styles/font-awesome/font-awesome.scss'], 'client', {
+		isImport: true
+	});
+	api.addFiles(['styles/_variables.scss', 'styles/_mixins.scss', 'styles/_forms.scss', 'styles/_buttons.scss'], 'client', {
+		isImport: true
+	});
 	api.add_files(['styles/styles.scss'], 'client');
 });
