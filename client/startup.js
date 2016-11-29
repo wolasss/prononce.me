39fc69340f9e5dbca85c0bf27eab297c25ffbd0e
Adam Wolski <wolas.abc@gmail.com>
2016-11-29 11:09:19 +0100

    recaptcha fix

--- client/startup.js
+++ client/startup.js
@@ -1,12 +1,12 @@
 Meteor.startup(function() {
-    reCAPTCHA.config({
-        theme: 'light',
-        publickey: YT_APP.Settings.Google.captcha_key
-    });
+	reCAPTCHA.config({
+		theme: 'light',
+		sitekey: YT_APP.Settings.Google.captcha_key
+	});
 	SEO.config({
 		title: TAPi18n.__("seo.global.title"),
 		meta: {
-		  'description': TAPi18n.__("seo.global.desc")
+			'description': TAPi18n.__("seo.global.desc")
 		}
 	});
 });
